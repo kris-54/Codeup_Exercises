e944553ab8f9655d70b37bb4657782f3330160b3
Kris C <kris541@hotmail.com>
2014-08-14 10:23:00 -0500

    do while loop

---
+++ do_while.php
@@ -0,0 +1,27 @@
+<?php
+
+//example
+// $a = 1;
+// do {
+//     echo "\$a is equal to {$a}\n";
+//     $a++;
+// } while ($a <= 5);
+// $a is equal to 1
+// $a is equal to 2
+// $a is equal to 3
+// $a is equal to 4
+// $a is equal to 5
+//-----------------------------------------------
+
+
+//create a do-while loop that will count by 2's starting with 0 and ending with 100.
+
+$a = 0;
+
+do {
+	echo "{$a}\n";
+	$a +=2;
+} while ($a <= 100);
+
+
+
