707b49e4d57b9345ac37e64386500e56834cde16
Kris C <kris541@hotmail.com>
2014-08-14 22:19:53 -0500

    fizzbuzz created with for loop, if/elseif statements

---
+++ fizzbuzz.php
@@ -0,0 +1,21 @@
+<?php
+
+for ($num = 1; $num <= 100; $num++) {
+	if (($num % 5 == 0) && ($num % 3 == 0)) {
+		echo "FIZZBUZZ" . PHP_EOL;
+	}
+	elseif ($num % 3 == 0){
+		echo "FIZZ". PHP_EOL;
+	}
+	elseif ($num % 5 == 0) {
+		echo "BUZZ" . PHP_EOL;
+	}
+	else {
+		echo "{$num}" . PHP_EOL;
+	}
+}
+
+
+
+
+?>
