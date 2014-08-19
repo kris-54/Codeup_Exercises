af518a812a70d3d77c7f6c39887b0f0463126b35
Kris C <kris541@hotmail.com>
2014-08-19 11:34:19 -0500

    created a function to check if variable is set

---
+++ internal_functions.php
@@ -0,0 +1,33 @@
+<?php
+
+
+
+// Create a function that checks if a variable is set or empty, and display "$variable_name is SET|EMPTY"
+
+// TEST: If var $nothing is set, display '$nothing is SET'
+
+// TEST: If var $nothing is empty, display '$nothing is EMPTY'
+
+// TEST: If var $something is set, display '$something is SET'
+
+// Serialize the array $array, and output the results
+
+// Unserialize the array $array, and output the results
+
+function variableCheck($a) {
+	if(isset($a)) {
+		echo "The variable is SET" . PHP_EOL;
+	} else {
+		echo "Variable is either zero or empty" . PHP_EOL;
+	}
+}
+
+$nothing = NULL;
+$something = ' ';
+$array = array(1,2,3);
+
+
+variableCheck($nothing);
+variableCheck($something);
+variableCheck($array);
+
