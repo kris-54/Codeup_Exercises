06feafd8e58756044ead2415d3f89d7ecc76c5b1
Kris C <kris541@hotmail.com>
2014-09-18 22:31:26 -0500

    1. deleted old commented out code 1. fixed spacing

--- do_while.php
+++ do_while.php
@@ -1,26 +1,12 @@
 <?php
 
-//example
-// $a = 1;
-// do {
-//     echo "\$a is equal to {$a}\n";
-//     $a++;
-// } while ($a <= 5);
-// $a is equal to 1
-// $a is equal to 2
-// $a is equal to 3
-// $a is equal to 4
-// $a is equal to 5
-//-----------------------------------------------
-
-
 //create a do-while loop that will count by 2's starting with 0 and ending with 100.
 
 $a = 0;
 
 do {
-	echo "{$a}\n";
-	$a +=2;
+    echo "{$a}\n";
+    $a +=2;
 } while ($a <= 100);
 
 
@@ -29,8 +15,8 @@ do {
 $a = 100;
 
 do {
-	echo "{$a}\n";
-	$a = $a - 5;
+    echo "{$a}\n";
+    $a = $a - 5;
 } while ($a >= -10);
 
 //create do while loop that starts at 2 and displays the result on each line while $a is less than 1000000
@@ -38,8 +24,8 @@ do {
 $a = 2;
 
 do {
-	echo "{$a}\n";
-	$a = $a * $a;
+    echo "{$a}\n";
+    $a = $a * $a;
 } while ($a < 1000000);
 
 
