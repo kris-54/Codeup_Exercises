42fe2bb7fa7c52682338ceae5ebeba83b7f1e453
Kris C <kris541@hotmail.com>
2014-09-18 22:36:38 -0500

    fixed spacing and deleted old comments

--- if.php
+++ if.php
@@ -1,52 +1,5 @@
 <?php
 
-//  $a = 5;
-//  $b = 10;
-//  $c = '10';
-
-//  if ($a < $b) {
-//      // output the appropriate result
-//      echo "$a is less than $b\n";
-//  }
-
-//  if ($b > $a) {
-//      // output the appropriate result
-//      echo "$b is greater than $a\n";
-//  }
-
-//  if ($b >= $c) {
-//      // output the appropriate result
-//      echo "$b is greater than or equal to $c\n";
-//  }
-
-//  if ($b <= $c) {
-//      // output the appropriate result
-//      echo "$b is less than or equal to $c\n";
-//  }
-
-//  if ($b == $c) {
-//      // output the appropriate result
-//      echo "$b is equal to $c\n";
-//  }
-
-//  if ($b === $c) {
-//      // output the appropriate result
-//      echo "$b is identical $c\n";
-//  }
-
-//  // output the appropriate result
-//  if ($b != $c) {
-//      echo "$b is not equal to $c\n";
-//  }
-
-//  // output the appropriate result
-//  if ($b !== $c) {
-//      echo "$b is not identical to $c\n";
-//  }
-
-
- 
-
 $a = 10;
 $b = 20; 
 $c = '2';
@@ -57,7 +10,7 @@ if ($a < $b) {
     echo "$b is greater than $a\n";
 }
 
-// Shorten the next 2 statements into an if/else
+
 if ($b >= $c) {
    echo "$b is greater than or equal to $c\n";
 } else {
