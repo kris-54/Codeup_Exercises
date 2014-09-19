f5c131310bc9079df1cb7a33164a79ffaf42f7c8
Kris C <kris541@hotmail.com>
2014-09-18 22:43:22 -0500

    fixed spacing and comments

--- search_arrays.php
+++ search_arrays.php
@@ -10,14 +10,14 @@
 Create a function that returns TRUE or FALSE if an array value is found. 
 Search for Tina and Bob in $names. Make sure it works as expected.*/
 
- function search($needle, $haystack)
+function search($needle, $haystack)
 {
- 	
-	if (array_search($needle, $haystack) === false) {
- 		return 'false' . PHP_EOL;
- 	} else {
- 		return 'true' . PHP_EOL;
- 	}
+    
+    if (array_search($needle, $haystack) === false) {
+        return 'false' . PHP_EOL;
+    } else {
+        return 'true' . PHP_EOL;
+    }
 }
 
 echo search('Bob', $names) . PHP_EOL;
@@ -28,14 +28,17 @@ the arrays. Use the 2 example arrays and make sure your solution uses array_sear
 
 function compare($array1, $array2)
 {
-	$common = 0;
-	foreach ($array1 as $key => $value) {
-		$search = array_search($value, $array2);
-		if(is_numeric($search)) {
-			$common++;
-		}
-	} 
-	return $common;  
+    $common = 0;
+    foreach ($array1 as $key => $value) {
+        $search = array_search($value, $array2);
+        if(is_numeric($search)) {
+            $common++;
+        }
+    } 
+    return $common;  
 }
 
 echo "There are " . compare($names,$compare) . " names in common" . PHP_EOL;
+
+
+?>
