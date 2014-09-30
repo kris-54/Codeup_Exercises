6f73d0ec73686b52b600f931621de9df9b514358
Kris C <kris541@hotmail.com>
2014-09-29 20:50:16 -0500

    fixed spacing and deleted extra lines

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,21 +1,16 @@
 <?php
 
-for ($num = 1; $num <= 100; $num++) {
-	if (($num % 5 == 0) && ($num % 3 == 0)) {
-		echo "FIZZBUZZ" . PHP_EOL;
-	}
-	elseif ($num % 3 == 0){
-		echo "FIZZ". PHP_EOL;
-	}
-	elseif ($num % 5 == 0) {
-		echo "BUZZ" . PHP_EOL;
-	}
-	else {
-		echo "{$num}" . PHP_EOL;
-	}
+for ($num = 1; $num <= 100; $num++) 
+{
+    if (($num % 5 == 0) && ($num % 3 == 0)) {
+        echo "FIZZBUZZ" . PHP_EOL;
+    } elseif ($num % 3 == 0) {
+        echo "FIZZ". PHP_EOL;
+    } elseif ($num % 5 == 0) {
+        echo "BUZZ" . PHP_EOL;
+    } else {
+        echo "{$num}" . PHP_EOL;
+    }
 }
-
-
-
 
 ?>
