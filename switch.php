c9853d8de3ef7177f91396691102879e5b9c3cd7
Kris C <kris541@hotmail.com>
2014-09-29 21:11:42 -0500

    changed tab spacing

--- switch.php
+++ switch.php
@@ -4,27 +4,27 @@ date_default_timezone_set('America/Chicago');
 
 $day_of_week = date('N');
 
-switch($day_of_week) {
-	
-	case 1:
-		echo "Today is Monday" . PHP_EOL;
-		break;
-	case 2:
-		echo "Today is Tuesday" . PHP_EOL;
-		break;
-	case 3:
-		echo "Today is Wednesday" . PHP_EOL;
-		break;
-	case 4:
-		echo "Today is Thursday" . PHP_EOL;
-		break;
-	case 5: 
-		echo "Today is friday" . PHP_EOL;
-		break;
-	case 6 : 
-		echo "Today is saturday" . PHP_EOL;
-		break;
-	case 7:
-		echo "Today is sunday" . PHP_EOL;
-		break;
+switch($day_of_week) 
+{
+    case 1:
+        echo "Today is Monday" . PHP_EOL;
+        break;
+    case 2:
+        echo "Today is Tuesday" . PHP_EOL;
+        break;
+    case 3:
+        echo "Today is Wednesday" . PHP_EOL;
+        break;
+    case 4:
+        echo "Today is Thursday" . PHP_EOL;
+        break;
+    case 5: 
+        echo "Today is friday" . PHP_EOL;
+        break;
+    case 6 : 
+        echo "Today is saturday" . PHP_EOL;
+        break;
+    case 7:
+        echo "Today is sunday" . PHP_EOL;
+        break;
 }
